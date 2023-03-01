cc0451c95aa3d8a5fb8fd737f7baf887cff99a52
OliverFang™ <hung0911248587@gmail.com>
2023-03-01 17:24:08 +0700

    hung

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Navbaar from './components/header/Navbaar';
 import Newnav from './components/newnavbaar/Newnav';
+import Maincomp from './components/home/MainComp';
 function App() {
+  
   return (
 
   <>
